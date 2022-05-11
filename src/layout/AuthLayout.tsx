9d720b745202510c899d6a8d42faebd4e7bfa9de
joaoaoluiz <joaoaoluiz@protonmail.com>
2022-05-11 20:57:11 -0300

    Add page title

--- src/layout/AuthLayout.tsx
+++ src/layout/AuthLayout.tsx
@@ -7,15 +7,25 @@ import routes from 'router/routes'
 
 function AuthLayout () {
   return (
-    <div className='flex flex-col h-full font-nunito'>
+    <div className='flex flex-col h-full max-h-full font-nunito'>
       <AuthLayoutContextProvider>
         <AppHeader />
-        <div className='relative flex grow'>
+        <div className='relative flex overflow-hidden grow'>
           <SideBar />
           <main className='w-full p-4 grow bg-dark-background'>
+
             {<Routes>
               {Object.values(routes.auth).map(route => (
-                <Route key={route.path} {...route} />
+                <Route
+                  {...route}
+                  key={route.path}
+                  element={
+                    <>
+                      <h1 className='text-xl font-bold uppercase text-light-text'>{route.label}</h1>
+                      {route.element}
+                    </>
+                  }
+                  />
               ))}
             </Routes>}
           </main>
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -6,7 +6,6 @@ function Home () {
   // TODO 🎈 grid auto-fit
   return (
     <section>
-      <h1 className='font-bold text-light-text'>INÍCIO</h1>
       <div className='grid grid-rows-3 gap-4 md:grid-cols-3'>
         {Object.entries(routes.auth)
           .filter(([routeName, routeConfig]) => routeConfig.inHome)
